dd9aca9cff0d6497695adbd1b89625d1f9663823
simei-yeh <simei.yeh@gmail.com>
2020-12-04 14:46:53 -0800

    Finished postgres sedding script

--- controllers/index.js
+++ controllers/index.js
@@ -3,16 +3,16 @@ const helpers = require('../db/models.js');
 function getSpecificListing(req, res) {
   helpers.listingModel.find({ id: req.params.id })
     .then((listings) => {
+      // eslint-disable-next-line linebreak-style
       res.header('Content-Type', 'application/json');
       res.send(JSON.stringify(listings, 0, 2));
     });
 }
 
-
-//must modify
+// must modify
 function addBooking(req, res) {
   helpers.listingModel.findOneAndUpdate({ id: req.params.id },
-    {$push: { reserved: req.body } }, { new: true} )
+    { $push: { reserved: req.body } }, { new: true })
     .then((listings) => {
       res.header('Content-Type', 'application/json');
       res.send(JSON.stringify(listings, 0, 2));
@@ -39,5 +39,5 @@ module.exports = {
   getSpecificListing,
   addBooking,
   modifyBooking,
-  deleteBooking
-}
+  deleteBooking,
+};
