168f4cd4bc4d6155927c75bca828b5f4066e5832
simei-yeh <simei.yeh@gmail.com>
2020-12-11 17:18:01 -0800

    Fixed stress tests

--- db/models.js
+++ db/models.js
@@ -1,7 +1,7 @@
-const { Pool } = require('pg');
+const { Client, Pool } = require('pg');
 const path = require('path');
 
-const pool = new Pool({
+const client = new Client({
   host: 'localhost',
   user: 'postgres',
   database: 'sdc',
@@ -12,8 +12,23 @@ const pool = new Pool({
 });
 // eslint-disable-next-line linebreak-style
 
-pool.connect()
-  .then((response) => { console.log('connected to postgres db!'); })
+const pool = new Pool({
+  host: 'localhost',
+  user: 'postgres',
+  database: 'sdc',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+client.connect()
+  .then((response) => { console.log('connected to postgres db for client!'); })
   .catch((err) => { console.log(err); });
 
+pool.connect()
+  .then((response) => { console.log('connected to postgres db for pool!'); })
+  .catch((err) => { console.log(err); });
+
+module.exports = client;
 module.exports = pool;
--- server/app.js
+++ server/app.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
-const Helpers = require('../db/models.js');
+// const morgan = require('morgan');
 const listings = require('../controllers/listing.js')
 const res = require('../controllers/reservations.js')
 
 const app = express();
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(express.json());
 // app.use('/api/listings/:id', express.static(path.join(__dirname, '..', 'client', 'dist')));
 app.use('/listings/:id', express.static(path.join(__dirname, '..', 'client', 'dist')));
@@ -20,6 +19,5 @@ app.delete('/api/listings/:id/deleteListing', listings.deleteListing);
 app.get('/api/listings/:id/getReservations', res.getReservation);
 app.post('/api/listings/:id/makeReservation', res.makeReservation);
 app.delete('/api/listings/:id/deleteReservation', res.deleteReservation);
-
 
 module.exports = app;
